37e03bdf31a31c6c96a66d9213aaf825ca13926f
CoderYounes <younesanas2021@gmail.com>
2023-06-15 12:02:44 +0100

    fix betty checks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - entrypoint
+ *
+ * Description: simple code print some text using puts
+ *
+ * Return: 0 always succes
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
