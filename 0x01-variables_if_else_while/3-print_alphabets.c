e1b6bec02f909ecd6c9b919ea62544368231f6a3
CoderYounes <younesanas2021@gmail.com>
2023-06-16 21:46:45 +0100

    print Upper & lower alphabets case using for loops

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: print alphabet Lower & Upper case using for loop
+ *
+ * Return: 0 Always success
+ *
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
