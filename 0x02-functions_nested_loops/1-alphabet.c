89cb7efeb04708609f5f49b2342d75e3b917b934
CoderYounes <younesanas2021@gmail.com>
2023-06-20 21:31:25 +0100

    fix some betty issue

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
 /**
+ * print_alphabet - function name
+ *
+ * Description: function print alphabets via putchar
+ *
+ * Return: nothing to return it's a void
  */
 
 void print_alphabet(void)
