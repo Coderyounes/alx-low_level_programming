9db24bf9ec718a48be3fd8966e05631c0c90a214
CoderYounes <younesanas2021@gmail.com>
2023-06-21 21:08:42 +0100

    fix datatype from int to long long int

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,18 +10,18 @@
 
 int main(void)
 {
-	int nums[50];
+	long long int nums[50];
 	int i;
 
 	nums[0] = 1;
 	nums[1] = 2;
 
-		printf("%d, %d ", nums[0], nums[1]);
+		printf("%lld, %lld ", nums[0], nums[1]);
 
 	for (i = 2; i < 50; i++)
 	{
 		nums[i] = nums[i - 1] + nums[i - 2];
-		printf("%d", nums[i]);
+		printf("%lld", nums[i]);
 
 		if (i != 49)
 		{
