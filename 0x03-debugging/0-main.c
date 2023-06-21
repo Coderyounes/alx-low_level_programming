4761a244f859b2958546eb1d26597da670f6be31
CoderYounes <younesanas2021@gmail.com>
2023-06-21 19:32:17 +0100

    main.c & main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - EntryPoint
+ *
+ * Description: Code  to test positive or negative function
+ *
+ * Return: 0 Always Success
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
