ee93e5c36b0ee219f82944e931c327fd5971e099
CoderYounes <younesanas2021@gmail.com>
2023-06-26 14:50:57 +0100

    reset to 98 task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function Declaration
+ *
+ * Description: Simple code reset n value to 98
+ *
+ * @n: integer to reset
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
