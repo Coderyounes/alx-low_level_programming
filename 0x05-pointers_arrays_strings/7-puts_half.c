3651ff06e61211a1dab382b420f3c50eda6bfb3f
CoderYounes <younesanas2021@gmail.com>
2023-06-26 22:30:24 +0100

    print the last half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - function name
+ *
+ * Description: print the last half of string
+ *
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+	int i, l, h;
+
+	for (l = 0; str[l] != '\0'; l++)
+		;
+
+	h = l / 2;
+
+	while (str[h] != '\0')
+	{
+		_putchar(*(str + h));
+		h++;
+	}
+	_putchar('\n');
+}
