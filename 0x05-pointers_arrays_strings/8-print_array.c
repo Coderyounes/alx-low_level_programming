e27aa05b7d56ad5d91749e6779c06d69ed4322f3
CoderYounes <younesanas2021@gmail.com>
2023-06-26 23:31:42 +0100

    reformulation

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,23 +12,15 @@
 
 void print_array(int *a, int n)
 {
-	int i = 0;
-
-	if(n  < 0)
-	{
-		return;
-	}
-
+	int i;
 
 	for (i = 0; i < n; i++)
 	{
+		printf("%d", a[i]);
 		if (i != n - 1)
 		{
-			printf("%d, ", a[i]);
-		}
-		else
-		{
-			printf("%d\n", a[i]);
+			printf(", ");
 		}
 	}
+	printf("\n");
 }
