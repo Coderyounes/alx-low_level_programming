bd1293cb4cc76e6fcfaaffc3855a85f2a528b781
CoderYounes <younesanas2021@gmail.com>
2023-07-04 00:10:29 +0100

    add betty comments

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,15 @@
 #include "main.h"
 
 /**
+ * _memset - function name
+ *
+ * @s: pointer to string
+ *
+ * @b: charachter to fill with
+ *
+ * @n: number of bytes to fill
+ *
+ * Return: new String
  *
  */
 
