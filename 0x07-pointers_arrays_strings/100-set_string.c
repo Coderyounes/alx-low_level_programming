57eff0efbbcd31f8a18b2f636a7f9e00b666d338
CoderYounes <younesanas2021@gmail.com>
2023-07-04 01:09:58 +0100

    Complet tasks

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * set_string - function set pointer value to char
+ *
+ * @s: pointer to pointer
+ *
+ * @to: pointer to char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
