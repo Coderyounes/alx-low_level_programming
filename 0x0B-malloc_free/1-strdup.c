994bc1db6c27f5ca8cb3003212f204fcbf9b4fb9
CoderYounes <younesanas2021@gmail.com>
2023-07-10 17:03:20 +0100

    second task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strdup - function name
+ *
+ * Description: Function mesure a variable & duplicate it
+ *
+ * @str: string
+ *
+ * Return: Null if there is issue, otherwise the new string
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i, j;
+	char *string;
+
+	if (!str)
+	{
+		return (NULL);
+	}
+
+	j = strlen(str);
+
+	string = malloc(j * sizeof(char));
+
+	if (!string)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < j; i++)
+	{
+		string[i] = str[i];
+	}
+	string[i] = '\0';
+
+	return (string);
+}
