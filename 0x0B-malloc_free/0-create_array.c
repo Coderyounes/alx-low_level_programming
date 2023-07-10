0f74037baf3719c003d1ef62f513a41d526d9943
CoderYounes <younesanas2021@gmail.com>
2023-07-10 16:39:21 +0100

    README file & Create ana array of char using malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_array - function to Allocate Memory for Array
+ *
+ * @size: pointer to the size of the array
+ *
+ * @c: pointer to char
+ *
+ * Return: pointer to array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *array;
+
+	if (size != 0)
+	{
+		return (NULL);
+	}
+
+	array = malloc(size * sizeof(char));
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+
+	return (array);
+}
