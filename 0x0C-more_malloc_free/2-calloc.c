60f42c267faa92a9e7b4f4636f9fe885f0e6dfbc
CoderYounes <younesanas2021@gmail.com>
2023-07-13 23:04:28 +0100

    calloc , can't solve this so I ask my partner

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _calloc - function to  initialize the allocated memory to 0
+ *
+ * @nmemb: elements number of the array
+ *
+ * @size: size in byte of each element
+ *
+ * Return: it's void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(nmemb * size);
+
+	if (!p)
+	{
+		memset(p, 0, nmemb * size);
+	}
+
+	return (p);
+
+
+}
