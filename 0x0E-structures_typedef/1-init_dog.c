214637404a2fa647176c86fa5cd56a530598e1eb
CoderYounes <younesanas2021@gmail.com>
2023-07-18 01:01:03 +0100

    fix some betty issue

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,9 +1,15 @@
 #include "dog.h"
 
 /**
- * init_dog - function to initialize d
+ * init_dog - struct dog initialization
  *
- *  
+ * @d: pointer to struct dog
+ *
+ * @name: dog name
+ *
+ * @age: dog age
+ *
+ * @owner: dog owner name
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
