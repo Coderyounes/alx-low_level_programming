c36fc3e76149acb323fb1b2e7411bdd1ad8632aa
CoderYounes <younesanas2021@gmail.com>
2023-07-21 00:14:39 +0100

    variadic function Print them all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,99 @@
+#include "variadic_functions.h"
+
+/**
+ * p_char - function to print char
+ * @c: char to print
+ * @separator: empty string until finish
+ */
+
+void p_char(int c, char *separator)
+{
+	printf("%s%c", separator, c);
+}
+
+/**
+ * p_integer - function to print integer
+ * @i: integer to print
+ * @separator: empty string until finish
+ */
+
+void p_integer(int i, char *separator)
+{
+	printf("%s%d", separator, i);
+}
+
+/**
+ * p_float - function to print floit
+ * @f: double type to print
+ * @separator: empty string until finish
+ */
+
+void p_float(double f, char *separator)
+{
+	printf("%s%f", separator, f);
+}
+
+/**
+ * p_str - function to print string
+ * @s:  pointer to string to print
+ * @separator: empty string until finish
+ */
+
+void p_str(char *s, char *separator)
+{
+	if (s == NULL)
+	{
+		printf("(nil)");
+	}
+	else
+	{
+		printf("%s%s", separator, s);
+	}
+}
+
+
+/**
+ * print_all - function to print all data type
+ *
+ * @format: identifier of each data type
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+
+	unsigned int i = 0;
+	char *separator = "";
+
+	va_start(args, format);
+
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				p_char(va_arg(args, int), separator);
+				break;
+			case 'i':
+				p_integer(va_arg(args, int), separator);
+				break;
+			case 'f':
+				p_float(va_arg(args, double), separator);
+				break;
+			case 's':
+				p_str(va_arg(args, char *), separator);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		separator = ", ";
+		i++;
+
+	}
+
+	va_end(args);
+	printf("\n");
+
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,6 +7,11 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void p_char(int c, char *separator);
+void p_integer(int i, char *separator);
+void p_float(double f, char *separator);
+void p_str(char *s, char *separator);
+void print_all(const char * const format, ...);
 
 
 #endif /* VARIADIC_FUNCTIONS_H */
