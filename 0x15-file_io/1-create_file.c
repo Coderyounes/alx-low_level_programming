303ba9aefec8b73eb82f4b7c7d850c02db4762cb
CoderYounes <younesanas2021@gmail.com>
2023-08-07 15:55:25 +0100

    fix the issue where : if the file already exist

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (0);
 
-	file = open(filename, O_CREAT | O_WRONLY, S_IWUSR | S_IRUSR);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IWUSR | S_IRUSR);
 	if (file < 0)
 		return (-1);
 
