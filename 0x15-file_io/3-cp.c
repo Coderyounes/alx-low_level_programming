449178c945479c44118a37efc367277cb2a400f4
CoderYounes <younesanas2021@gmail.com>
2023-08-08 23:27:46 +0100

    Code reformulation use a simple version

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,121 +1,51 @@
 #include "main.h"
 
 /**
- * main - Copies the content of a file to another file
- * @argc: Number of arguments passed to the program
- * @argv: Array of arguments
+ * main - Entry point
  *
- * Return: 0 on success, 97, 98, 99, or 100 on error
+ * @argc: number of argument
+ *
+ * @argv: argument vector
+ *
+ * Return: 0 on Success, otherwise exit
  */
 int main(int argc, char *argv[])
 {
-	int src_fd, dest_fd, close_src, close_dest;
+	int source, dest, read_bytes, write_bytes;
+	char buf[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-	src_fd = open_source_file(argv[1]);
-	if (src_fd < 0)
-		exit(98);
-
-	dest_fd = open_dest_file(argv[2]);
-	if (dest_fd < 0)
-	{
-		close(src_fd);
-		exit(98);
-	}
-
-	if (copy_file(src_fd, dest_fd, argv) != 0)
-	{
-		close(src_fd);
-		close(dest_fd);
-		exit(99);
-	}
-
-	close_src = close(src_fd);
-	close_dest = close(dest_fd);
-
-	if (close_src < 0 || close_dest < 0)
-	{
-		if (close_src < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src_fd);
-		if (close_dest < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_fd);
-		exit(100);
-	}
-
-	exit(0);
-}
-/**
- * open_source_file - Function open source file
- *
- * @filename: pointer to destination file
- *
- * Return: opened file
- */
-int open_source_file(const char *filename)
-{
-	int fd = open(filename, O_RDONLY);
-
-	if (fd < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-	}
-	return (fd);
-}
-
-/**
- * open_dest_file - function to open dest file
- *
- * @filename: pointer to file
- *
- * Return: the opened file
- */
-int open_dest_file(const char *filename)
-{
-	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	if (fd < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-	}
-	return (fd);
-}
-
-/**
- * copy_file - function copy content of file to another
- *
- * @src_fd: source file
- *
- * @dest_fd: destination file
- *
- * @argv: pointer to argument vector
- *
- * Return: 0 on Success , otherwise -1
- */
-int copy_file(int src_fd, int dest_fd, char *argv[])
-{
-	char buffer[1024];
-	ssize_t bytes_read;
-
-	while ((bytes_read = read(src_fd, buffer, sizeof(buffer))) > 0)
-	{
-		if (write(dest_fd, buffer, bytes_read) != bytes_read)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			return (-1);
-		}
-	}
-
-	if (bytes_read < 0)
+	source = open(argv[1], O_RDONLY);
+	if (source < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (-1);
+		exit(98);
 	}
+
+	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (dest < 0)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+
+	while ((read_bytes = read(source, buf, 1024)) > 0)
+	{
+		write_bytes = write(dest, buf, read_bytes);
+		if (write_bytes < 0)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+
+	if (read_bytes < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	if (close(source) < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source), exit(100);
+
+	if (close(dest) < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest), exit(100);
 
 	return (0);
 }
-
