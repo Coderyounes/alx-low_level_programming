e13167a6e1756d816e5cbc9ad62b1a50fa0ac2ee
CoderYounes <younesanas2021@gmail.com>
2023-09-14 22:11:32 +0100

    let's try again

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -14,10 +14,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	for (i = 0; i < index && head->next != NULL; i++)
-	{
+	for (i = 0; head && i < index; i++)
 		head = head->next;
-	}
 
 	return (head);
 }
