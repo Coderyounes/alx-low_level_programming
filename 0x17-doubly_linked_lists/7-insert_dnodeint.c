8d8e9284cced38c00908b66252cbcfff103042d4
CoderYounes <younesanas2021@gmail.com>
2023-09-14 23:21:20 +0100

    update linked list based on index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Function insert node at certain position
+ *
+ * @h: pointer to pointer tofirst node of the liked list
+ *
+ * @idx: position of certain node
+ *
+ * @n: integer data to add in node
+ *
+ * Return: New nodes address
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *newnode;
+	dlistint_t *temp;
+	unsigned int i;
+
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL || h == NULL)
+		return (NULL);
+
+	newnode->n = n;
+	newnode->next = NULL;
+	newnode->prev = NULL;
+
+	temp = *h;
+
+	if (idx == 0)
+	{
+		newnode->next = *h;
+		(*h)->prev = newnode;
+		*h = newnode;
+		return (newnode);
+	}
+		for (i = 0; i < idx && temp != NULL ; i++)
+		{
+			if (i == idx - 1)
+			{
+				newnode->next = temp->next;
+				newnode->prev = temp;
+				temp->next = newnode;
+				return (newnode);
+			}
+			else
+				temp = temp->next;
+		}
+
+	return (NULL);
+
+}
